d55a8019a094dbef5cefacdf3cc536a2357ce829
ianjravin <ianjravin@gmail.com>
2018-03-02 13:19:53 +0300

    stop being nosey

--- php/send-contact.php
+++ php/send-contact.php
@@ -1,16 +1,17 @@
 <?php
 
-	$fname = @trim(stripslashes($_POST['fname']));
+	$name = @trim(stripslashes($_POST['name']));
 	$email = @trim(stripslashes($_POST['email']));
-	$phone = @trim(stripslashes($_POST['phone']));
-	$comment = @trim(stripslashes($_POST['comments']));
+	$subject = @trim(stripslashes($_POST['subject']));
+
+	$message = @trim(stripslashes($_POST['message']));
 
 	$email_from = $email;
-	$email_to = 'ian@nouveta.tech';//replace with your email
+	$email_to = 'info@zaka.co.ke';//replace with your email
 
-	$body = 'Name: ' . $fname . "\n\n" . 'Email: ' . $email . "\n\n" . 'Phone: ' . $phone .  "\n\n" . 'Comments: ' . $comments;
+	$body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" .  'Message: ' . $message;
 
-	$success = @mail($email_to, $body, 'Name: ' . $fname . "\n\n" . 'Email: ' . $email . "\n\n" . 'Phone: ' . $phone . "\n\n" . 'Comments: ' . $comments);
+	$success = @mail($email_to, $body, 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message);
 
 ?>
 
